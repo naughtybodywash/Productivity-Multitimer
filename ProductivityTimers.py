0dca841920bf6af6ffc6777b5699dd107f71e357
Josh Babin <josh.babin@gmail.com>
2021-01-01 22:41:16 -0600

    Alter functionality substituted for datetime function in TK. Some syntactical changes for printouts. Comparison operators corrected. Need to update pom time from 60 to 25*60. Label updated to left align.

--- ProductivityTimers.py
+++ ProductivityTimers.py
@@ -23,27 +23,32 @@ pom = Label(root, text="Start Pomodoro Timer")
 pom.grid()
 
 # Set the size of the window
-root.geometry('350x200') 
+root.geometry('350x200')
 
 # Define hydrate button function
-def hydration(): 
-    messagebox.showinfo("Water", "Hydrate Now!!") 
+def hydration():
+    messagebox.showinfo("Water", "Hydrate Now!!")
     # Next line to be uncommented when the timer itself is built
     water.configure(text="Hydration timer running...")
-    
+
+# Define t_now reassignment function
+def tnowreassign():
+    t_now = dt.datetime.now
+    return t_now
+
 # Define pomodoro button function
-def pomodoro(): 
+def pomodoro():
     # Create time variables
     t_now = dt.datetime.now()
-    t_pom = 25*60
+    t_pom = 60
     t_delta = dt.timedelta(0, t_pom)
     t_run = t_now + t_delta
     t_brk = 5*60
     t_pomend = t_now + dt.timedelta(0, t_pom + t_brk)
-    
-    #Messagebox
+
+    #messagebox
     pom.configure(text="Timer running...")
-    
+
     total_pomodoros = 0
     breaks = 0
     # Pomodoro loop
@@ -51,18 +56,19 @@ def pomodoro():
         # Pomodoro clock running
         if t_now < t_run:
             print("Pomodoro")
-        elif t_delta <- t_now <- t_pomend:
+            #pom.configure(text= t_pom-t_delta)
+        elif t_run <= t_now <= t_pomend:
             # ring a bell if it's the first time here
             print("In break")
             if breaks == 0:
-                pom.configure(text=("On break!!!"))
+                pom.configure(text="On break!!!")
                 # annoying bell
                 for i in range(5):
                     winsound.Beep((i+100), 700)
                 print("Break time!")
                 breaks += 1
         #Pomodoro and break are completed
-        else: 
+        else:
             print("Finished!")
             # Reset breaks
             breaks = 0
@@ -80,30 +86,40 @@ def pomodoro():
                 continue
             elif usr_ans == False:
                 #user done...display stats
-                messagebox.showinfo("Pomodoro finished!", "\nYou completed"+
+                messagebox.showinfo("Pomodoro finished!", "\nYou completed "+
                                     str(total_pomodoros)+ " pomodoros today!")
+                pom.configure(text="Start Pomodoro Timer")
                 break
         #Check every 20 seconds and update current time
         print('sleeping')
-        time.sleep(20)
+        root.after(20000, tnowreassign) # Need to swap out time.sleep with a tk.after function
         t_now = dt.datetime.now()
         timenow = t_now.strftime("%H:%M")
 
 # Create a separate thread for the pomodoro timer
-def start_pom_thread(event):
+def start_pom_thread():
     global pom_thread
     pom_thread = threading.Thread(target=pomodoro)
     pom_thread.daemon = True
     #progressbar.start()
     pom_thread.start()
 
+# def start_hydrate_thread():
+#     global hydrate_thread
+#     hydrate_thread = threading.Thread(target=hydrate)
+#     pom_thread.daemon = True
+#     pom_thread.start()
+
 
 #Create hydrate button
-btnH = Button(root, text="Start", command=hydration)
-btnP = Button(root, text="Start", command=lambda:start_pom_thread(None))
+btnHyd = Button(root, text="Start", command=hydration)
+btnPom = Button(root, text="Start", command=start_pom_thread)
 
-btnH.grid(column=1, row=0)
-btnP.grid(column=1, row=1)
+water.grid(sticky=W, column=0, row=0)
+pom.grid(column=0, row=1)
+
+btnHyd.grid(column=1, row=0)
+btnPom.grid(column=1, row=1)
 
 # Initialize the window and keep it open until we close it with "X" button
 # Takes all widgets and objects, render them, and respond to interactions
